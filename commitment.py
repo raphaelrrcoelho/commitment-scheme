74c2a884a26d1855670f17fb49096c0701bf9f02
kapital <kapital@localhost.localdomain>
2017-02-11 03:58:26 -0200

    fix key so it have always 64 characters lenght

--- commitment.py
+++ commitment.py
@@ -5,10 +5,11 @@ from random import getrandbits
 def main():
     if argv[1] == 'commit':
         msg = argv[2]
-        key = str(getrandbits(256))
+        key = "{0:0{1}x}".format(getrandbits(256), 64) 
+        print(len(key))
         com = hashing(key + msg)
-        print("com: %s" % com)
-        print("key: %s" % key)
+        print("com: %s lenght: %i" % (com, len(com)))
+        print("key: %s lenght: %i" % (key, len(key)))
 
     if argv[1] == 'verify':
         com = argv[2]
@@ -18,6 +19,7 @@ def main():
 
 def hashing(value):
     encoded_value = value.encode()
+    print(sha256(encoded_value).hexdigest())
     return sha256(encoded_value).hexdigest()
 
 if __name__ == "__main__":
